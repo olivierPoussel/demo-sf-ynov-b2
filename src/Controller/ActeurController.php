bcf5b2f87b32eb907f64651cf179d44aa82dabbc
olivier poussel <olivier.poussel@gmail.com>
2022-05-02 17:46:01 +0200

    save 02/05/22

---
+++ src/Controller/ActeurController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Acteur;
+use App\Form\ActeurType;
+use App\Repository\ActeurRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/acteur')]
+class ActeurController extends AbstractController
+{
+    #[Route('/', name: 'acteur_list')]
+    public function acteurList(ActeurRepository $acteurRepository): Response
+    {
+        $acteurList = $acteurRepository->findAll();
+
+        return $this->render('/acteur/list.html.twig', [
+            "listActeur" => $acteurList
+        ]);
+    }
+
+    #[Route('/create', 'acteur_create')]
+    public function createActeur(Request $request, ManagerRegistry $doctrine)
+    {
+        //afficher le form
+        $acteur = new Acteur();
+        $form = $this->createForm(ActeurType::class, $acteur);
+        //recup des datas
+        $form->handleRequest($request);
+
+        //check si le form est submit
+        if ($form->isSubmitted() && $form->isValid()) {
+            //les envoyer a la bdd
+            $manager = $doctrine->getManager();
+            $manager->persist($acteur);
+            $manager->flush();
+
+            return $this->redirectToRoute('acteur_list');
+        }
+
+
+        return $this->render('/acteur/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'acteur_detail')]
+    public function acteurDetail($id, ActeurRepository $acteurRepository)
+    {
+        $acteur = $acteurRepository->find($id);
+
+        return $this->render('/acteur/detail.html.twig', [
+            "acteur" => $acteur
+        ]);
+    }
+}
